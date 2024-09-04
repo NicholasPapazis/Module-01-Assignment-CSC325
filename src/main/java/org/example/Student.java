c0998ea30ff4a036316f838ac4a3e7eba6f74562
NicholasPapazis <nicholasepapazis@gmail.com>
2024-09-04 18:31:37 -0400

    updated Student class

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -10,8 +10,19 @@ public class Student {
 
 
     //To Do 01: add a constructor to this class
+    public Student(String name){
+        this.name = name;
+    }
 
     // To Do 02: Add setters and getters
+    //getter
+    public String getName(){
+        return name;
+    }
+    //setter
+    public void setName(String name){
+        this.name = name;
+    }
 
 
 
